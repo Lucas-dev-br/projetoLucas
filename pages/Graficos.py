dd4a7ea83297fba876527c35209ec76976912be4
Lucas Henrique Araujo Vasconcelos <85376643+Lucas-dev-br@users.noreply.github.com>
2023-12-15 16:38:06 -0300

    feat: the lasts alterations

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -11,37 +11,37 @@ except FileNotFoundError:
     st.stop()
 
 # Display the data
-st.title("Product and Price Analysis")
-st.subheader("Preview of the Data:")
+st.title("Analise de produto e preços")
+st.subheader("Pré-visualização dos dados:")
 st.write(df_product_prices.head())
 
 # Interactive filter for selecting products
-selected_product = st.selectbox("Select a Product:", df_product_prices['productname'].unique())
+selected_product = st.selectbox("Selecione o produto:", df_product_prices['productname'].unique())
 
 # Filter data for the selected product
 filtered_data = df_product_prices[df_product_prices['productname'] == selected_product]
 
 # Visualization options
-visualization_option = st.radio("Select Visualization Type:", ["Line Chart", "Bar Chart", "Scatter Plot"])
+visualization_option = st.radio("Selecione o tipo de visualização:", ["Gráfico de linha", "Gráfico de barras", "Gráfico de dispersão"])
 
 # Interactive filter for selecting cities
-selected_cities = st.multiselect("Select Cities:", filtered_data.columns[2:6])
+selected_cities = st.multiselect("Selecionar as cidades:", filtered_data.columns[2:6])
 
 # Plot selected visualization
-st.subheader(f"{visualization_option} for {selected_product} in Selected Cities:")
-if visualization_option == "Line Chart":
+st.subheader(f"{visualization_option} para {selected_product} nas cidades selecionadas:")
+if visualization_option == "Gráfico de linha":
     fig = px.line(filtered_data, x='date', y=selected_cities, title=f"{selected_product} Prices Over Time")
-elif visualization_option == "Bar Chart":
+elif visualization_option == "Gráfico de barras":
     fig = px.bar(filtered_data, x='date', y=selected_cities, title=f"{selected_product} Prices Over Time")
-elif visualization_option == "Scatter Plot":
+elif visualization_option == "Gráfico de dispersão":
     fig = px.scatter(filtered_data, x='date', y=selected_cities, title=f"{selected_product} Prices Over Time")
 
 # Show the interactive chart
 st.plotly_chart(fig)
 
 # Summary statistics
-st.subheader("Summary Statistics:")
+st.subheader("Estatísticas resumidas:")
 if selected_cities:
     st.write(filtered_data[selected_cities].describe())
 else:
-    st.warning("Please select at least one city for summary statistics.")
+    st.warning("Porfavor selecione ao menos uma cidade para mostrar o grafico.")
